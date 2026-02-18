b0ea2b734896053fbb770b4662ef7f9a00006897
bridgettebeaton <bridgette.beaton@du.edu>
2026-02-18 14:32:29 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('#learnMoreBtn");
+const extraText = document.querySelector('#extraText');
+
+button.addEventListener("click", () => {
+  extraText.style.display = "block";
+  button.textContent = "Thanks for Visiting";
+});
